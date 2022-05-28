ffad9b8306504341ee78e51b520b77f705d7881f
George Ciobanu <george.adrian.ciobanu@gmail.com>
2022-05-28 13:29:09 +0300

    h_sh_list(ch_04)

---
+++ ch_04/homework_shopping_list(ch_04).py
@@ -0,0 +1,11 @@
+
+name = input("Hi! Whats your name?: ")
+age = input("How old are you?: ")
+shopping_list = str(input("What would you like to buy?: "))
+counter = int(shopping_list.count(""))
+
+if 0.0 <= counter <= 1:
+    print(f"{name}, please return with a shopping list.\n Thank you!")
+elif counter > 0:
+    print(f"{name}, please wait for {counter} seconds.\n Thank you for your patience!")
+
