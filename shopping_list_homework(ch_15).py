a96f865f23e4e40de2e1f81075fd4e125a100aef
George Ciobanu <george.adrian.ciobanu@gmail.com>
2022-06-15 11:44:39 +0300

    homework

--- shopping_list_homework(ch_15).py
+++ shopping_list_homework(ch_15).py
@@ -1,75 +1,48 @@
 import random
-from types import NoneType
 user_greetings_prompts = ["Hi!","Hello!","Greetings, traveler!"]
 user_info_prompts = ["What's your age? ","How old are you? ","Please tell me your age. "]
 user_shopping_list_prompts = ["What's on your shopping list? ","What would you like to buy? ","What can I get you? "]
 user_farewell_prompts = ["Thank you for your purchase!","Thanks! Please come again.","Thank you!"]
 
+def get_customer_name():    
+    name = input(f"{random.choice(user_greetings_prompts)} \n Please enter your name here: ")
+    return name 
 
-name = input(f"{random.choice(user_greetings_prompts)} \n Please enter your name here: ")
+def get_customer_age():       
+    age = int(input("Please enter your age: ")) 
+    return age
 
-
-
-age = int(input("Please enter your age: ")) 
-
-
-
-shopping_list = list()
-
-while True:
-    print("Please enter your item here: ")
-    groceries = input()
-    shopping_list.append(groceries)       
+def get_shopping_list(age):
     
-    if groceries == " " or groceries == "nothing":
-        break
-
-
-
-
-print(f"Hi {name}! \n Welcome to our store!")
- 
+    not_allowed = ("beer", "wine", "rum", "cigarettes")
+    shopping_list = list()
     
-if age < 18:
-
-    try:          
-        number_groceries = len(shopping_list)
-        seconds = (number_groceries -2) * 30 
-        # if number_groceries > 1:                      
-                                    
-        if "cigarettes" in shopping_list:
-            print("You cannot buy cigarettes. I will remove them from your list.")
-            shopping_list = shopping_list.remove("cigarettes") 
-            seconds = number_groceries - 1
-        elif "beer" in shopping_list:
-            print("You cannot buy beer. I will remove it from your list.")
-            shopping_list = shopping_list.remove("beer") 
-            seconds = number_groceries - 1
-        elif "wine" in shopping_list:
-            print("You cannot buy wine. I will remove it from your list.")
-            shopping_list = shopping_list.remove("wine") 
-            seconds = number_groceries - 1
-        elif "rum" in shopping_list:
-            print("You cannot buy rum. I will remove it from your list.")
-            shopping_list = shopping_list.remove("rum") 
-            seconds = number_groceries - 1
-        else:
-            pass
-                
-        print(f"We're really sorry {name}, but '{random.choice(shopping_list)}'j is no longer available. \n You will have to wait 30 seconds for every item in your list. \n Please wait for {seconds} seconds. \n Have a great day, {name}!")
-#in mod normal *random.choice(shopping_list)* scoate un produs aleator din lista, dar daca varsta clientului este sub 18 ani
-#si acesta alege sa cumpere un produs interzis din lista, totul merge normal, mai putin *random.choice(shopping_list)*   
-    except TypeError:
-        print(f"We're really sorry {name}, but one of your items is no longer available. \n You will have to wait 30 seconds for every item in your list. \n Please wait for {seconds} seconds. \n Have a great day, {name}!")
+    while True:
+        print("Please enter your item here: ")
+        groceries = input()
+       
+        if groceries == " " or groceries == "nothing":
+            break
+        if age < 18:
+            if groceries in not_allowed:
+                print(f"You can't buy {groceries}, I will not add it to your list.")
+                continue    
+        shopping_list.append(groceries)
+    return shopping_list
         
-else:
-    
+
+def main():
+    name = get_customer_name()
+    print(f"Hi {name}! \n Welcome to our store!")
+    age = int(get_customer_age()) 
+    shopping_list = get_shopping_list(age) 
+       
     number_groceries = len(shopping_list)
-    seconds = (number_groceries -2) * 30 
+    seconds = number_groceries * 30 
     if number_groceries <= 1:
         print("Please enter a shopping list!")
     else:            
-        print(f"We're really sorry {name}, but {random.choice(shopping_list)} is no longer available. \n You will have to wait 30 seconds for every item in your list. \n Please wait for {seconds} seconds. \n Have a great day, {name}!")
+        print(f"We're really sorry {name}, but '{random.choice(shopping_list)}' is no longer available. \n You will have to wait 30 seconds for every item in your list. \n Please wait for {seconds} seconds. \n Have a great day, {name}!")
 
 
-
+main()
